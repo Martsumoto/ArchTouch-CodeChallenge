38e10165c851f7f12d430d9348b173f84fa1b261
Marcelo Matsumoto <marcelokmats@gmail.com>
2019-04-01 19:35:26 -0300

    adding some tests

---
+++ app/src/test/java/com/arctouch/codechallenge/util/ResponseError.kt
@@ -0,0 +1,9 @@
+package com.arctouch.codechallenge.util
+
+import okhttp3.MediaType
+import okhttp3.ResponseBody
+import retrofit2.Response
+
+fun buildResponse() : Response<Error> {
+    return Response.error(403, ResponseBody.create(MediaType.parse("application/json"), ""))
+}
